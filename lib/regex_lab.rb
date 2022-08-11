0444c0aeb0ef98701d70ca4aea3703c2109a9092
Michael Dear <michaelcdear@gmail.com>
2022-08-11 12:38:12 -0600

    commit tree 6c22ae879ac61601e41d83742cdd12095acea67d

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,13 +1,14 @@
 def starts_with_a_vowel?(word)
-
+    word.split("")[0].match(/[aeiouAEIOU]/) ? true : false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+    text.scan(/un[a-z]*ing/)
 end
 
 def words_five_letters_long(text)
-
+    p text
+    p text.scan(/[a-z]{5}/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
@@ -17,3 +18,5 @@ end
 def valid_phone_number?(phone)
 
 end
+
+starts_with_a_vowel?("yrank")
